1656feec400db8290a27f0057992aa249318bc9a
Ahmed Rayan <ahmedrayan587@gmail.com>
2024-07-10 10:13:38 +0300

    fix database connection errors

--- server/index.js
+++ server/index.js
@@ -7,17 +7,14 @@ const app = express();
 configDotenv();
 app.use(cors());
 app.use(express.json());
-/*the database did not connect from the device not the code */
-/*mongoose
-  .connect(process.env.MONGO_URL, () => {
-    useNewUrlParser: true;
-  })
+mongoose
+  .connect(process.env.MONGO_URL)
   .then(() => {
     console.log("DB Connected");
   })
   .catch((err) => {
     console.log(err.message);
-  });*/
+  });
 app.listen(process.env.PORT, () => {
   console.log(`listening on port ${process.env.PORT}`);
 });
