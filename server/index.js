00786209e3f6efd18276355aaf637019f1ab1554
Ahmed Rayan <ahmedrayan587@gmail.com>
2024-07-10 05:40:12 +0300

    creat front with vite, back with init and i express mongoose nodemon socket.io bcrypt cors dotenv for back and axios styled-components react-router-dom for the front

---
+++ server/index.js
@@ -0,0 +1,23 @@
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+import { configDotenv } from "dotenv";
+
+const app = express();
+configDotenv();
+app.use(cors());
+app.use(express.json());
+/*the database did not connect from the device not the code */
+mongoose
+  .connect(process.env.MONGO_URL, () => {
+    useNewUrlParser: true;
+  })
+  .then(() => {
+    console.log("DB Connected");
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
+app.listen(process.env.PORT, () => {
+  console.log(`listening on port ${process.env.PORT}`);
+});
